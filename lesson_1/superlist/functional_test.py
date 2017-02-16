f106d6951b65be383fa62caeef333ac57b5707a0
Ferenc Kondasz <ferenc@getbynder.com>
2017-02-16 16:51:26 +0100

    Updated functional test

--- lesson_1/superlist/functional_test.py
+++ lesson_1/superlist/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # User sees "Django" in the page title
-        self.assertIn('To-Do lists', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').textbox
+        self.assertIn('To-Do', header_text)
 
         # User invited to enter a to-do list item straight away
-
-        # User types "My to do" into the textbox
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # User types "My to-do" into the textbox
+        inputbox.send_keys('My to-do')
 
         # When user hits ENTER, the page updates
         # "1: My to-do" as an item in the to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: My to-do' for row in rows)
+        )
 
         # There is still a textbox inviting the user to add an other item.
         # User enters "My second to do"
+        self.fail('Finish the test!')
 
         # Page updates again and shows both items
 
